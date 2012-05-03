4fe251b18e839c67115e41c95f5d039b4b317b10
unknown <FireBone@PC.(none)>
2012-05-03 13:06:34 +0200

    lol

--- XMPPTest/src/TestServer/ServerCommander.java
+++ XMPPTest/src/TestServer/ServerCommander.java
@@ -5,66 +5,85 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-/*
-	public void conversazione() {
-		String message = "";
-		BufferedReader t = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			out.println("Connected! write /logout to log out ");
-			while(!message.equals("/logout")) {
-				message = readBuffer(reader);
-				System.out.println(message);
-				if(!message.equals("/logout")) {
-					message = t.readLine();
-					out.println(" scrive: "+message);
-				}
-			} // while
-		}
-		catch(IOException e) {
-			System.out.println("End conversation");
-		}
-	} 
-}*/
-	
-
 	public class ServerCommander implements Server{
 		static char[] s = new char[10000];
+		static private HashMap<Integer,Thread> ConnectionList=new HashMap<Integer,Thread>();
 		private static Socket connection;
-		private static BufferedReader reader;
-		private static PrintWriter writer;
-	  	public ServerCommander() throws IOException{
+	  	public ServerCommander() {
 	  		//TODO
 	  	}
 	  	
-		public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException{
-			//ServerCommander server=new ServerCommander(); //si richiamerà poi server.qualcosa per dare i comandi
-			try{
-				ServerSocket socket = new ServerSocket(5222);
-				System.out.println("Server on");
-				connection=socket.accept();//in teoria dovrebbe esserci sempre un serverSocket,magari su un altro thread,in attesa di un nuovo client che si vuole connettere,TODO
-				System.out.println("Connection estabilished with: "+connection.getInetAddress().getHostAddress()+"\nport: "+connection.getPort());
+//Connection Listener
+	private static class ConnectionListener extends Thread {
+		public ConnectionListener(){}
+
+		@Override
+		public void run() {
+			while(true){
+				try{
+					ServerSocket socket = new ServerSocket(5222);
+					connection=socket.accept();
+					System.out.println("Connection estabilished with: "+connection.getInetAddress().getHostAddress()+"\nport: "+connection.getPort());
+					ConnectionRunner cr=new ConnectionRunner(connection);
+					ConnectionList.put(cr.hashCode(),cr);
+					ConnectionList.get(cr.hashCode()).start();
+				}catch(Exception e){}
+			}
+		}
+	}
+	
+	private static class ConnectionRunner extends Thread {
+		private Socket connection;
+		private BufferedReader reader;
+		private PrintWriter writer;
+		boolean end=false;
+		public ConnectionRunner(Socket socket){
+			connection=socket;
+		}
+		
+		@Override
+		public void run() {
+			try {
 				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 				System.out.println("created reader");
 				writer = new PrintWriter(connection.getOutputStream());
 				System.out.println("created writer");
-		  	}catch(Exception e){System.err.println(e);System.exit(-1);}//avevo scritto System.orr di orrori ma non me l'ha preso :(
-		  	System.out.println("no exeption");
-		  	String handshake=readBuffer(reader);
-		  	System.out.println(handshake);
-		  	StringTokenizer sc=new StringTokenizer(handshake,"'\"");
-		  	if(!(sc.hasMoreTokens()&&sc.nextToken().equals("<stream:stream to=")))throw new IllegalArgumentException();
-		  	sc.nextToken();//if(!sc.hasMoreTokens()||!sc.nextToken().matches("[/d]{0,3}[/.][/d]{0,3}[/.][/d]{0,3}"))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().equals(" xmlns="))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().matches("[j][a][b][b][e][r][:][^/d]*"))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().equals(" xmlns:stream="))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().equals("http://etherx.jabber.org/streams"))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().equals(" version="))throw new IllegalArgumentException();
-		  	if(!sc.hasMoreTokens()||!sc.nextToken().equals("1.0"))throw new IllegalArgumentException();
-		  	String handshake2="<stream:stream from='"+connection.hashCode()+"' xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>";
-		  	writer.write(handshake2);
-		  	writer.flush();
-		  	String auth=readBuffer(reader);
-		  	System.out.println(auth);
+				System.out.println("no exeption");
+		  		String handshake=readBuffer(reader);
+		  		System.out.println(handshake);
+		  		StringTokenizer sc=new StringTokenizer(handshake,"'\"");
+		  		if(!(sc.hasMoreTokens()&&sc.nextToken().equals("<stream:stream to=")))throw new IllegalArgumentException();
+		  		sc.nextToken();//if(!sc.hasMoreTokens()||!sc.nextToken().matches("[/d]{0,3}[/.][/d]{0,3}[/.][/d]{0,3}"))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().equals(" xmlns="))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().matches("[j][a][b][b][e][r][:][^/d]*"))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().equals(" xmlns:stream="))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().equals("http://etherx.jabber.org/streams"))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().equals(" version="))throw new IllegalArgumentException();
+		  		if(!sc.hasMoreTokens()||!sc.nextToken().equals("1.0"))throw new IllegalArgumentException();
+		  		String handshake2="<stream:stream from='"+connection.hashCode()+"' xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>";
+		  		writer.write(handshake2);
+		  		writer.flush();
+		  		String auth=readBuffer(reader);
+		  		System.out.println(auth);
+			}catch (IllegalArgumentException e){throw new IllegalArgumentException();}
+			catch (Exception e) {}
+			while(!end){
+				try {
+					String lettura=readBuffer(reader);
+					
+				} catch (Exception e) {}
+			}
+		}
+		
+		@Override
+		public int hashCode(){
+			//TODO: ogni utente una sola connessione 
+			return connection.hashCode();
+		}
+	}
+	  	
+		public static void main(String[] args) {
+			ConnectionListener cl=new ConnectionListener();
 		  	while(true);//test
 		  	/*writer.close();
 			reader.close();
